fa88b1eda88f0bf2aee500cd2d57aef39ca3eb19
Guillem Orellana <guillem.orellana@gmail.com>
2018-08-17 15:31:05 +0200

    Sorry i am noob

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(methodOverride());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
