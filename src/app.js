5a31e0def7f1276db3999b3fa81c9b12ff3f94e0
Guillem Orellana <guillem.orellana@gmail.com>
2018-08-17 15:40:34 +0200

    Specify methods CORS

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(methodOverride());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
